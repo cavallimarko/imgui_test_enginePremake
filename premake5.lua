2b46250e042c5dc3b6169c0872239674273a2e3d
Marko Cavalli <cavalli.marko@gmail.com>
2026-03-02 20:43:58 +0100

    premake

---
+++ premake5.lua
@@ -0,0 +1,47 @@
+project "ImGuiTestEngine"
+    kind "StaticLib"
+    language "C++"
+    cppdialect "C++20"
+    staticruntime "off"
+
+    targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "imgui_test_engine/*.h",
+        "imgui_test_engine/*.cpp",
+        "imgui_test_engine/thirdparty/**.h",
+        "imgui_test_engine/thirdparty/**.cpp",
+        "shared/*.h",
+        "shared/*.cpp"
+    }
+
+    includedirs
+    {
+        "%{IncludeDir.ImGui}",
+        "%{IncludeDir.ImGuiTestEngine}",
+        "%{IncludeDir.ImGuiTestShared}",
+        "%{IncludeDir.glm}"
+    }
+
+    defines
+    {
+        "IMGUI_ENABLE_TEST_ENGINE"
+    }
+
+    filter "system:windows"
+        systemversion "latest"
+
+    filter "configurations:Debug"
+        runtime "Debug"
+        symbols "on"
+
+    filter "configurations:Release"
+        runtime "Release"
+        optimize "on"
+
+    filter "configurations:Dist"
+        runtime "Release"
+        optimize "on"
+
